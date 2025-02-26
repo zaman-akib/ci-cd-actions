9a6278820a585bb958454f310317573aaeae699e
Akib Uz Zaman <akibuz@blackduck.com>
2025-02-27 00:53:08 +0600

    add message for no issues or pull requests found

--- script/main.py
+++ script/main.py
@@ -3,6 +3,8 @@ from repo_info import *
 if __name__ == "__main__":
     OWNER = "jenkinsci"
     REPO = "jenkins"
+    PAGE_SIZE = 20
+
     fetch_latest_commit(OWNER, REPO)
-    fetch_issues(OWNER, REPO)
-    fetch_pull_requests(OWNER, REPO)
+    fetch_issues(OWNER, REPO, PAGE_SIZE)
+    fetch_pull_requests(OWNER, REPO, PAGE_SIZE)
--- script/repo_info.py
+++ script/repo_info.py
@@ -19,9 +19,10 @@ def fetch_latest_commit(owner, repo):
     date = latest_commit['commit']['author']['date']
     print(f"Latest Commit sha: {sha}\nMessage: {message}\nAuthor: {author}, Date: {date}\n")
 
-def fetch_issues(owner, repo):
+def fetch_issues(owner, repo, page_size):
     url = f"https://api.github.com/repos/{owner}/{repo}/issues"
-    params = {'state': 'open', 'per_page': 10}
+    params = {'state': 'open', 'per_page': page_size}
+    issue_count = 0
 
     print(f"{'='*50}\nList of Issues\n{'='*50}")
 
@@ -32,25 +33,38 @@ def fetch_issues(owner, repo):
             sys.exit(1)
 
         issues = response.json()
+        if not issues:
+            print("No open issues found")
+
+        issue_count += len(issues)
+        print(f"Fetching issues {issue_count - len(issues) + 1}-{issue_count}...")
+
         for issue in issues:
             print(f"Issue: {issue['title']}, Status: {issue['state']}")
 
         url = response.links.get('next', {}).get('url')
 
-def fetch_pull_requests(owner, repo):
+def fetch_pull_requests(owner, repo, page_size):
     url = f"https://api.github.com/repos/{owner}/{repo}/pulls"
-    params = {'state': 'open', 'per_page': 10}
+    params = {'state': 'open', 'per_page': page_size}
+    pr_count = 0
 
     print(f"{'='*50}\nList of Pull Requests\n{'='*50}")
 
     while url:
-     response = requests.get(url, params=params)
-     if response.status_code != 200:
+        response = requests.get(url, params=params)
+        if response.status_code != 200:
          print(f"Error: Unable to fetch pull requests. Status code: {response.status_code}")
          sys.exit(1)
 
-     pulls = response.json()
-     for pr in pulls:
+        pulls = response.json()
+        if not pulls:
+            print("No open pull requests found")
+
+        pr_count += len(pulls)
+        print(f"Fetching pull requests {pr_count - len(pulls) + 1}-{pr_count}...")
+
+        for pr in pulls:
          print(f"Pull Request: {pr['title']}, Status: {pr['state']}")
 
-     url = response.links.get('next', {}).get('url')
+        url = response.links.get('next', {}).get('url')
